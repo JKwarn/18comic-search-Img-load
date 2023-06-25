5b89256761cbb413cccc34215058d9b99a53b004
名氏无 <ceshizhuanyong@hotmail.com>
2023-06-25 16:36:39 +0800

    修改元属性以取消脚本的警告信息

--- user.js
+++ user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Comic图片加载
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       acer
 // @match        https://18comic.vip/search/*
-// @include      https://18comic.org/search/*
-// @updateURL    https://github.com/JKwarn/18comic-search-Img-load/blob/main/user.js
+// @match        https://18comic.org/search/*
+// @downloadURL  https://github.com/JKwarn/18comic-search-Img-load/blob/main/user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=18comic.vip
 // @grant        none
 // ==/UserScript==
